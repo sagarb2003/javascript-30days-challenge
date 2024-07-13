d1fdc2b0d92ca50288e3a035cca0012bb17d29c6
sagarb2003 <sagarssb2003@gmail.com>
2024-07-14 00:19:11 +0530

    solved-july-13

---
+++ July13/test.js
@@ -0,0 +1,34 @@
+//Activity -1 
+
+var num=10;
+let str="sagar";
+// console.log(num);
+// console.log(str);
+
+//Activity-2
+const assignBool=true;
+// console.log(assignBool);
+
+const obj={
+    name:'sagar',
+    age:'20'
+}
+// const arr=[1,2,3,4];
+
+//Activity-3
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof assignBool);
+// console.log(typeof obj);
+// console.log(typeof arr);
+
+//Activity-4
+let str1="sagar"
+console.log(str1);
+str1="bisht"
+console.log(str1);
+
+//Activity-5
+
+// assignBool=false; gives error as constant variable can't be changed
+
