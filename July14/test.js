4e42798d8c50c627d4b2e173b4f8a794ae819953
sagarb2003 <sagarssb2003@gmail.com>
2024-07-14 18:22:16 +0530

    solved-july-14

---
+++ July14/test.js
@@ -0,0 +1,37 @@
+//Activity-1 :Arithmetic operators
+const a=10;
+const b=17;
+// console.log(`Sum ${a+b}`);
+// console.log(`Sub ${a-b}`);
+// console.log(`Multiply ${a+b}`);
+// console.log(`Divide ${a/b}`);
+// console.log(`Remainder ${b%a}`);
+
+//Activity-2 : Assignment operators
+let c=5;
+c+=10;
+let d=10;
+d-=5;
+// console.log(`Sum ${c}`);
+// console.log(`Sub ${d}`);
+
+//Activity-3 : Comparison Operators
+
+// console.log(3<5);
+// console.log(3>5);
+// console.log(3>=5);
+// console.log(5<=5);
+// console.log(5==5);
+// console.log('5'==5); true
+// console.log('5'===5); false checks data types also
+
+//Activity-4 : Logical Operators
+let e=12;
+console.log(e%3==0 && e%2==0);
+let f=15;
+console.log(f%5 ==0 || f<10);
+console.log(!(f % 5 == 0));
+
+//Activity-5 : Ternary Operators
+let g=-27;
+console.log(g>0 ? 'Positive Number' : 'Negative Number');
